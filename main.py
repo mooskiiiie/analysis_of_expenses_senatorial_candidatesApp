30dbb27836b724aa4c560c2535cd280cf15e2bba
Sebastian Vergara <73338501+mooskiiiie@users.noreply.github.com>
2021-11-29 15:14:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import warnings
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from multiapp import MultiApp
 from apps import home, links, methods, info, conclusion, cluster, analysis
 warnings.filterwarnings('ignore')
